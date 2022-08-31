30a3873b52992953046f5bb8107c1348b15e6e6e
Dominik Köhler <dominik.koehler90@gmail.com>
2022-08-31 18:56:00 +0200

    add initial script version

---
+++ GW2LaunchScript.bat
@@ -0,0 +1,14 @@
+set GW2Dir=E:\Games\ArenaNet\Guild Wars 2
+set GW2BinDir=%GW2Dir%\bin64
+set wgetPath=%ProgramFiles(X86)%\GnuWin32\bin\wget
+set ArcDpsWebsite=https://www.deltaconnected.com/arcdps/x64/
+
+:: Download for DX11
+wget -N --no-check-certificate "%ArcDpsWebsite%/d3d11.dll" -P "%GW2Dir%"
+
+:: Download for DX9
+::wget -N --no-check-certificate -O "%GW2BinDir%\d3d9.dll" "%ArcDpsWebsite%/d3d11.dll"
+
+::echo d | xcopy /y "%GW2BinDir%\d3d9.dll" "%GW2Dir%\d3d11.dll"
+
+start "" "%GW2Dir%\gw2-64.exe" -mapLoadinfo
